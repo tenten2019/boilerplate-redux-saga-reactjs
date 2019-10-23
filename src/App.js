72233b2eb762098ff50baabfbeea9d927710e266
Tai.vn <taivn@ghn.vn>
2019-10-23 15:40:15 +0700

    counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
-function App() {
+function App({ insc, insync, count }) {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>Demo 1</h1>
+      <p>{count}</p>
+      <span onClick={insc}>click</span>
+      <br />
+      <span onClick={insync}>click sync</span>
     </div>
   );
 }
 
-export default App;
+function mapState(state) {
+  return {
+    count: state.count,
+  }
+}
+
+function mapDispatch(dispatch) {
+  return {
+    insc: () => {dispatch({type: 'In',payload: 5})},
+    insync: () => {dispatch({type: 'In_Async'})},
+  }
+}
+
+export default connect(mapState, mapDispatch)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { put, takeEvery, delay} from 'redux-saga/effects'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initState = {
+  count: 0
+}
+
+const rootReducer = (state = initState, actions) => {
+  switch(actions.type) {
+    case 'In':
+      return {...state, count: state.count + actions.payload}
+    default:
+      return state
+  }
+}
+
+function* inasync() {
+  yield delay(500)
+  yield put({type: 'In',payload: 6})
+}
+
+function* rootSaga() {
+  yield takeEvery('In_Async', inasync)
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
